3d7a2215b646778735ebd33ed776f4f0d72e75d2
Mohammad Smadi <mo.smadi@outlook.com>
2022-05-04 17:59:54 +0300

    add enemy table

---
+++ Insert tblEnemy.sql
@@ -0,0 +1,29 @@
+USE [DoctorWho]
+GO
+
+INSERT INTO dbo.tblEnemy (EnemyId ,EnemyName ,Description)
+     VALUES (1 ,'archenemy' ,'archenemy evil');
+
+INSERT INTO dbo.tblEnemy (EnemyId ,EnemyName ,Description)
+     VALUES (2 ,'archfoe' ,'archfoe evil')
+
+INSERT INTO dbo.tblEnemy (EnemyId ,EnemyName ,Description)
+     VALUES (3 ,'attacker' ,'attacker evil')
+
+INSERT INTO dbo.tblEnemy (EnemyId ,EnemyName ,Description)
+     VALUES (4 ,'nemesis' ,'nemesis evil')
+
+INSERT INTO dbo.tblEnemy (EnemyId ,EnemyName ,Description)
+     VALUES (5 ,'bane' ,'bane evil')
+
+
+GO
+
+
+/*
+archenemy, 
+archfoe,
+attacker,
+nemesis,
+bane
+*/
