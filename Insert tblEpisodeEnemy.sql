fa302958ff9bcdb2f14d9ad9f7bbc09b3322b4e1
Mohammad Smadi <mo.smadi@outlook.com>
2022-05-04 20:29:16 +0300

    add EpisodeEnemy table

---
+++ Insert tblEpisodeEnemy.sql
@@ -0,0 +1,24 @@
+USE [DoctorWho]
+GO
+
+	 INSERT INTO dbo.tblEpisodeEnemy (EpisodeEnemyId ,EpisodeId ,EnemyId)
+     VALUES (1 ,1 ,1)
+
+
+	 INSERT INTO dbo.tblEpisodeEnemy (EpisodeEnemyId ,EpisodeId ,EnemyId)
+     VALUES (2 ,2 ,2)
+
+
+	 INSERT INTO dbo.tblEpisodeEnemy (EpisodeEnemyId ,EpisodeId ,EnemyId)
+     VALUES (3 ,3 ,3)
+
+
+	 INSERT INTO dbo.tblEpisodeEnemy (EpisodeEnemyId ,EpisodeId ,EnemyId)
+     VALUES (4 ,4 ,4)
+
+	 
+	 INSERT INTO dbo.tblEpisodeEnemy (EpisodeEnemyId ,EpisodeId ,EnemyId)
+     VALUES (5 ,5 ,5)
+GO
+
+
