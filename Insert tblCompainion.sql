19abfb6aa21d2321c3960f006414a66ad83f00d8
Mohammad Smadi <mo.smadi@outlook.com>
2022-05-04 20:29:57 +0300

    add Compainion table

---
+++ Insert tblCompainion.sql
@@ -0,0 +1,22 @@
+USE [DoctorWho]
+GO
+
+	 INSERT INTO dbo.tblCompainion (CompainionId ,CompainionName ,WhoPlayed)
+     VALUES (1 ,'Buddy' ,'WhoPlayed')
+
+
+	 INSERT INTO dbo.tblCompainion (CompainionId ,CompainionName ,WhoPlayed)
+     VALUES (2 ,'Bear' ,'WhoPlayed')
+
+
+	 INSERT INTO dbo.tblCompainion (CompainionId ,CompainionName ,WhoPlayed)
+     VALUES (3 ,'Zeus' ,'WhoPlayed')
+
+
+	 INSERT INTO dbo.tblCompainion (CompainionId ,CompainionName ,WhoPlayed)
+     VALUES (4 ,'Shadow' ,'WhoPlayed')
+
+
+	 INSERT INTO dbo.tblCompainion (CompainionId ,CompainionName ,WhoPlayed)
+     VALUES (5 ,'Angel' ,'WhoPlayed')
+GO
