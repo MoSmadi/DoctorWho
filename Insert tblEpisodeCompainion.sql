f0b72b23ad11030df09723c0bff75057c098b58a
Mohammad Smadi <mo.smadi@outlook.com>
2022-05-04 21:03:18 +0300

    modify the record

--- Insert tblEpisodeCompainion.sql
+++ Insert tblEpisodeCompainion.sql
@@ -1,29 +1,33 @@
 USE [DoctorWho]
 GO
 
-	 INSERT INTO dbo.tblEpisodeCompainion (EpisodeCompainionId ,EpisodeId ,CompainionId)
-     VALUES (1 ,1 ,1)
+	INSERT INTO dbo.tblEpisodeCompainion (EpisodeCompainionId ,EpisodeId ,CompainionId)
+    VALUES (1 ,1 ,1)
 
 
-	 INSERT INTO dbo.tblEpisodeCompainion (EpisodeCompainionId ,EpisodeId ,CompainionId)
-     VALUES (2 ,2 ,2)
+	INSERT INTO dbo.tblEpisodeCompainion (EpisodeCompainionId ,EpisodeId ,CompainionId)
+    VALUES (2 ,2 ,2)
 
 
-	 INSERT INTO dbo.tblEpisodeCompainion (EpisodeCompainionId ,EpisodeId ,CompainionId)
-     VALUES (3 ,3 ,3)
+	INSERT INTO dbo.tblEpisodeCompainion (EpisodeCompainionId ,EpisodeId ,CompainionId)
+    VALUES (3 ,3 ,3)
 
 
-	 INSERT INTO dbo.tblEpisodeCompainion (EpisodeCompainionId ,EpisodeId ,CompainionId)
-     VALUES (4 ,4 ,4)
+	INSERT INTO dbo.tblEpisodeCompainion (EpisodeCompainionId ,EpisodeId ,CompainionId)
+    VALUES (4 ,4 ,4)
 
 
-	 INSERT INTO dbo.tblEpisodeCompainion (EpisodeCompainionId ,EpisodeId ,CompainionId)
-     VALUES (5 ,5 ,5)
+	INSERT INTO dbo.tblEpisodeCompainion (EpisodeCompainionId ,EpisodeId ,CompainionId)
+    VALUES (5 ,5 ,5)
 
 
-	 UPDATE dbo.tblEpisodeCompainion
-	 SET CompainionId = 4
-	 WHERE EpisodeCompainionId = 5 and EpisodeId = 5;
+	UPDATE dbo.tblEpisodeCompainion
+	SET CompainionId = 4
+	WHERE EpisodeCompainionId = 5 and EpisodeId = 5;
+	 
+	UPDATE dbo.tblEpisodeCompainion
+	SET EpisodeId = 4
+	WHERE CompainionId = 3;
 GO
 
 
