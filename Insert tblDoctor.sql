2b8476886d4fe355c757573746dde0e981728493
Mohammad Smadi <mo.smadi@outlook.com>
2022-05-04 18:01:22 +0300

    add Doctor table

---
+++ Insert tblDoctor.sql
@@ -0,0 +1,26 @@
+USE [DoctorWho]
+GO
+
+INSERT INTO dbo.tblDoctor (DoctorId ,DoctorNumber ,DoctorName ,BirthDate ,FistEpisodeDate ,LastEpisodeDate)
+     VALUES (1  ,00 ,'Bader' ,'2000-11-11' ,'2010-04-11' ,'2011-11-11')
+
+
+INSERT INTO dbo.tblDoctor (DoctorId ,DoctorNumber ,DoctorName ,BirthDate ,FistEpisodeDate ,LastEpisodeDate)
+     VALUES (2  ,11 ,'Rami' ,'11-12-2000' ,'2011-05-12' ,'2012-11-11')
+
+
+INSERT INTO dbo.tblDoctor (DoctorId ,DoctorNumber ,DoctorName ,BirthDate ,FistEpisodeDate ,LastEpisodeDate)
+     VALUES (3  ,22 ,'Abed' ,'2000-01-11' ,'2012-06-13' ,'2013-11-11')
+
+
+INSERT INTO dbo.tblDoctor (DoctorId ,DoctorNumber ,DoctorName ,BirthDate ,FistEpisodeDate ,LastEpisodeDate)
+     VALUES (4  ,33 ,'Omar' ,'2000-02-11' ,'2013-07-14' ,'2014-11-11')
+
+
+INSERT INTO dbo.tblDoctor (DoctorId ,DoctorNumber ,DoctorName ,BirthDate ,FistEpisodeDate ,LastEpisodeDate)
+     VALUES (5  ,44 ,'Majd' ,'2000-03-11' ,'2014-08-15' ,'2015-11-11')
+
+
+GO
+
+
