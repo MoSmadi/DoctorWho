2b0e13f15b269cc0f4290b2cba1a02bfbb61db0b
Mohammad Smadi <mo.smadi@outlook.com>
2022-05-04 18:00:42 +0300

    add author table

---
+++ Insert tblAuthor.sql
@@ -0,0 +1,22 @@
+USE [DoctorWho]
+GO
+
+INSERT INTO dbo.tblAuthor (AuthorId ,AuthorName)
+     VALUES (1 ,'Mohammad')
+
+INSERT INTO dbo.tblAuthor (AuthorId ,AuthorName)
+     VALUES (2 ,'Ahmad')
+
+INSERT INTO dbo.tblAuthor (AuthorId ,AuthorName)
+     VALUES (3 ,'Isam')
+
+INSERT INTO dbo.tblAuthor (AuthorId ,AuthorName)
+     VALUES (4 ,'Zaid')
+
+INSERT INTO dbo.tblAuthor (AuthorId ,AuthorName)
+     VALUES (5 ,'Osaid')
+
+
+GO
+
+
