20ea7b9cbcb41d2bc737fd1f290d62de14c737a9
MoSmadi <mo.smadi@outlook.com>
2022-06-02 16:49:59 +0300

    modify the function from returning table to string

--- Requirement Number 7.sql
+++ Requirement Number 7.sql
@@ -1,21 +1,33 @@
 USE [DoctorWho]
 
-
 GO
-	CREATE FUNCTION fnEnemies  (@EpisodeId int)
-	RETURNS TABLE AS
-	RETURN
-		SELECT EnemyName 
-		FROM (tblEpisodeEnemy as EpisodeEnemy
-		INNER JOIN tblEnemy as Enemy ON EpisodeEnemy.EnemyId = Enemy.EnemyId)
-		WHERE Enemy.EnemyId =  @EpisodeId;
+	DROP FUNCTION IF EXISTS fnEnemies;
 GO
 
-select * from dbo.fnEnemies(1)
-select * from dbo.fnEnemies(2)
-select * from dbo.fnEnemies(3)
-select * from dbo.fnEnemies(4)
-select * from dbo.fnEnemies(5)
+	CREATE FUNCTION fnEnemies (@EpisodeId int)
+	RETURNS  varchar(100) AS
+		BEGIN
+			declare @EnemyNameList varchar(100) = ''
+
+			SELECT  @EnemyNameList = @EnemyNameList + case 
+														  when len(@EnemyNameList) > 0 
+														  THEN ', ' 
+														  ELSE '' 
+													  END + EnemyName 
+			FROM (tblEpisodeEnemy as EpisodeEnemy
+			INNER JOIN tblEnemy as Enemy ON EpisodeEnemy.EnemyId = Enemy.EnemyId)
+			WHERE Enemy.EnemyId =  @EpisodeId;
+
+			RETURN @EnemyNameList
+		END
+
+Go
+
+select dbo.fnEnemies(1)
+select dbo.fnEnemies(2)
+select dbo.fnEnemies(3)
+select dbo.fnEnemies(4)
+select dbo.fnEnemies(5)
 
 
 /*
